816b025e97a9d2ff7a96c52fae2cb72010b1a183
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2024-12-18 13:24:18 +0530

    js foundation

---
+++ 01-basics/operations.js
@@ -0,0 +1,14 @@
+//basic operations of js
+
+let add = 2 + 3;
+let subtract = 5 - 2;
+let multiply = 2 * 3;
+let divide = 10 / 5;
+let remainder = 10 % 2;
+let expo = 2 ** 4;
+
+//increment and decrement;
+
+let myscore = 110;
+myscore++;
+myscore--;
