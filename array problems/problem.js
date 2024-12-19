8a45a077861fab40dae344bcf3ffda53935b4494
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2024-12-19 17:35:30 +0530

    create problem.js

---
+++ array problems/problem.js
@@ -0,0 +1,20 @@
+/* Problem 1
+
+
+let teaFlavors = ["green tea", "black tea", "oolang tea"];
+
+var firsTea = teaFlavors[1];
+console.log(firsTea); */
+
+//Problem 2
+
+/* let teaTypes = ["white tea", "herbal tea", "masala chai"];
+teaTypes[2] = "jasmine tea";
+console.log(teaTypes);
+ */
+
+//Problem 3
+
+let citiesVisited = ["mumbai", "sydney"];
+citiesVisited.push = "berlin";
+console.log(citiesVisited);
