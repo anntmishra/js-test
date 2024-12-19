8a4cadf6720fbf8284b6d45c6c5ab193488d9ee3
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2024-12-19 14:04:32 +0530

    update

---
+++ conditional-problems/test.js
@@ -0,0 +1,51 @@
+//checking if number is greater than another number
+
+/*
+let num1 = 10;
+let num2 = 8;
+if (num1 > num2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+*/
+
+//checking if a string is equal to another string
+
+// let fullName = "anant mishra";
+// let firstName = "anant";
+// let lastName = "mishra";
+
+// let fullNameParts = fullName.split(" ");
+// let extractedFirstName = fullNameParts[0];
+// let extractedLastName = fullNameParts[1];
+
+// if (firstName === extractedFirstName && lastName === extractedLastName) {
+//   console.log("Same same");
+// } else {
+//   console.log("not same same");
+// }
+
+//
+// let fullName = "anant mishra";
+// let firstName = "anant";
+// let lastName = "mishra";
+
+// let split = fullName.split(" ");
+// let extractedName = split[0];
+// let extractedName2 = split[1];
+
+// if (firstName == extractedName && lastName == extractedName2) {
+//   console.log("same same");
+// } else {
+//   console.log("not same same");
+// }
+
+//checking if variable is a number or not
+
+let score = 22;
+if (typeof score == "number") {
+  console.log("yesss");
+} else {
+  console.log("No");
+}
