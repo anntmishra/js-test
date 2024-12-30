b3d7588e83aa034a7d185661d12b7d4fb1d00fc8
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2024-12-30 20:10:39 +0530

    update

---
+++ 04-loops/loops.js
@@ -0,0 +1,41 @@
+/* //Question 1
+ let sum = 0;
+let i = 1;
+
+while (i <= 5) {
+  sum +=i; //sum=sum+i
+  i++;
+}
+console.log(sum);
+ */
+
+/* //Question2 
+let countdown = [];
+let j = 15;
+while (j > 0) {
+  countdown.push(j);
+  j--;
+}
+console.log(countdown);
+ */
+
+/* //Question 3
+let teacollection = [];
+let tea;
+do {
+  tea = prompt(`Enter your favourite tea(type)`);
+  if (tea !== "stop") {
+    teacollection.push(tea);
+  }
+} while (tea !== "stop");
+ */
+
+//Question 4
+
+let multipliedNumbers = [];
+let numbers = [2, 4, 6];
+
+for (let i = 0; i < numbers.length; i++) {
+  multipliedNumbers.push(numbers[i] * 2);
+}
+console.log(multipliedNumbers);
--- array problems/problem.js
+++ array problems/problem.js
@@ -1,70 +1,72 @@
 /* Problem 1
-
+ */
 
 let teaFlavors = ["green tea", "black tea", "oolang tea"];
 
 var firsTea = teaFlavors[1];
-console.log(firsTea); */
+console.log(firsTea);
 
 //Problem 2
 
-/* let teaTypes = ["white tea", "herbal tea", "masala chai"];
+let teaTypes = ["white tea", "herbal tea", "masala chai"];
 teaTypes[2] = "jasmine tea";
 console.log(teaTypes);
- */
+
 
 //Problem 3
 
-/* let citiesVisited = ["mumbai", "sydney"];
+let citiesVisited = ["mumbai", "sydney"];
 citiesVisited.push("berlin");
 console.log(citiesVisited);
- */
+
 
 //Problem 4
-/* let teaOrders = ["chai", "iced tea", "matcha", "early grey"];
+let teaOrders = ["chai", "iced tea", "matcha", "early grey"];
 
 let firstOrder = teaOrders.splice(1, 1)[0];
 
 console.log(teaOrders);
 console.log(firstOrder);
- */
+
 
 //Problem 5
 
-/* let popularTeas = ["green tea", "oolang tea", "chai"];
-let softCopyTeas = popularTeas.slice();  --->(This is how we create softcopy of an array )
+let popularTeas = ["green tea", "oolang tea", "chai"];
+let softCopyTeas = popularTeas.slice();  /* --->(This is how we create softcopy of an array ) */
 console.log(softCopyTeas);
 
-******important= .slice()
- */
+/* ******important= .slice() */
+
 
 //Problem 6
 
-/* let topCities = ["berlin", "singapore", "new york"];
+let topCities = ["berlin", "singapore", "new york"];
 let hardCopyCities = [...topCities];
 topCities.pop;
 console.log(hardCopyCities);
- */
+
 
 //Problem 7
 
-/* let europianCities = ["paris", "rome"];
+let europianCities = ["paris", "rome"];
 let asianCities = ["tokyo", "bangkok"];
 // const newCitites = europianCities + "," + asianCities;//making it as a string we want a array
 // console.log(newCitites);
 
 //use of (.concat)
 let worldCities = europianCities.concat(asianCities);
-console.log(worldCities); */
+console.log(worldCities);
 
 //Problem 8
-/* let teaMenu = ["masala chai", "oolang tea", "green tea", "earl grey"];
+let teaMenu = ["masala chai", "oolang tea", "green tea", "earl grey"];
 
 const menuLength = teaMenu.length;
 console.log(menuLength);
- */
+
 
 //Problem 9
 let cityBucketList = ["kyoto", "london", "cape town", "vancouver"];
 const isLondoninList = cityBucketList.includes("london");
 console.log(isLondoninList);
+
+//use of (.includes)--->>> to check 
