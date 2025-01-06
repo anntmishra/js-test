e50ab9b50f4d3b86f7be9e700bcec1b88b286d6e
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2025-01-06 20:37:34 +0530

    Update loops.js

--- 04-loops/loops.js
+++ 04-loops/loops.js
@@ -30,12 +30,22 @@ do {
 } while (tea !== "stop");
  */
 
-//Question 4
-
+/* //Question 4
 let multipliedNumbers = [];
 let numbers = [2, 4, 6];
 
-for (let i = 0; i < numbers.length; i++) {
+for (let i = 0; i < 3; i++) {
   multipliedNumbers.push(numbers[i] * 2);
 }
 console.log(multipliedNumbers);
+ */
+
+//Question 5
+let myCities = [];
+const city = ["Paris", "New York", "Tokyo", "London"];
+
+for (let i = 0; i < city.length; i++) {
+  myCities.push(city[i]);
+}
+
+console.log(myCities);
